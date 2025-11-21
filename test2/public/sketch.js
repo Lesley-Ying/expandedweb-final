1f9aa7bffb4e17cc6bd169325d0f1b440c5d18ce
Lesley Ying <sy4405@nyu.edu>
2025-11-21 10:22:12 +0800

    Update sketch.js

--- test2/public/sketch.js
+++ test2/public/sketch.js
@@ -57,7 +57,7 @@ function drawMouth(indexList, face, x, y) {
   let w = (maxX - minX) + pad * 2;
   let h = (maxY - minY) + pad * 2;
   image(video, x, y, w * 2, h * 2, minX, minY, w, h);
-  filter(BLUR, 30); 
+  filter(POSTERIZE, 3); 
 }
 
 function gotFaces(results) {
